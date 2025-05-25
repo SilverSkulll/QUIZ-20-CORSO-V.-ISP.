b24bae0c0dbf5692b03267109598a9fbb7aa33fa
SilverSkulll <danielegiana@gmail.com>
2025-05-25 18:24:45 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -128,6 +128,11 @@ function App() {
               else if (opt === userAnswer && userAnswer !== correct) className += " wrong";
               if (opt === correct && userAnswer !== correct) className += " correct";
             }
+            if (userAnswer) {
+              if (opt === userAnswer && userAnswer === correct) className += ' correct';
+              else if (opt === userAnswer && userAnswer !== correct) className += ' wrong';
+              if (opt === correct && userAnswer !== correct) className += ' correct';
+            }
             return (
               <div key={opt} className={className} onClick={() => handleAnswer(opt)}>
                 <strong>{opt}.</strong> {value}
